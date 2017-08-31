8b1fbcb40e94e8956300985cb742ee282c1399db
Alexzev <alex@Alezev>
2017-08-31 14:12:58 +0300

    31.08.17-14:10

--- students/views/contact_admin.py
+++ students/views/contact_admin.py
@@ -7,6 +7,7 @@ from django.core.urlresolvers import reverse
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from django.views.generic.edit import FormView
 
 from studentsdb.settings import ADMIN_EMAIL
 
@@ -47,35 +48,56 @@ class ContactForm(forms.Form):
 
 
 
-def contact_admin(request):
+#def contact_admin(request):
     # check if form was posted
-    if request.method == 'POST':
+#    if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = ContactForm(request.POST)
+#        form = ContactForm(request.POST)
     
         # check whether user data is valid:
-        if form.is_valid():
+#        if form.is_valid():
         
             # send email
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            from_email = form.cleaned_data['from_email']
+#            subject = form.cleaned_data['subject']
+#            message = form.cleaned_data['message']
+#            from_email = form.cleaned_data['from_email']
             
-            try:
-                send_mail(subject, message, from_email, [ADMIN_EMAIL])
+#            try:
+#                send_mail(subject, message, from_email, [ADMIN_EMAIL])
             
-            except Exception:
-                message = u'Во время отправки письма возникла непредвиденная ошибка. Попробуйте воспользоваться данной формой позже.'
+#            except Exception:
+#                message = u'Во время отправки письма возникла непредвиденная ошибка. Попробуйте воспользоваться данной формой позже.'
             
-            else:
-                message = u'Сообщение успешно отправленно!'
+#            else:
+#                message = u'Сообщение успешно отправленно!'
             
             # redirect to same contact page with success message
-            return HttpResponseRedirect(
-                u'%s?status_message=%s' % (reverse('contact_admin'),message))
+#            return HttpResponseRedirect(
+#                u'%s?status_message=%s' % (reverse('contact_admin'),message))
     
     #if there was not POST render blank form
-    else:
-        form = ContactForm()
+#    else:
+#        form = ContactForm()
     
-    return render(request,'contact_admin/form.html', {'form': form})
+#    return render(request,'contact_admin/contact_form.html', {'form': form})
+
+class ContactView(FormView):
+    template_name = 'contact_admin/contact_form.html'
+    form_class = ContactForm
+    success_url = 'home'
+    def form_valid(self, form):
+        """This method is called for valid data"""
+        subject = form.cleaned_data['subject']
+        message = form.cleaned_data['message']
+        from_email = form.cleaned_data['from_email']
+        
+        try:
+            send_mail(subject, message, from_email, [ADMIN_EMAIL])
+        except Exception:
+            message = u'Во время отправки письма возникла непредвиденная ошибка. Попробуйте воспользоваться данной формой позже.'
+        else:
+            message = u'Сообщение успешно отправленно!'
+        return HttpResponseRedirect(
+            u'%s?status_message=%s' % (reverse('contact_admin'),message))
+    
+    #return super(ContactView, self).form_valid(form)
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
+from students.views.students import StudentUpdateView
+from students.views.contact_admin import ContactView
+
 from .settings import MEDIA_ROOT, DEBUG
+
 
 urlpatterns = patterns('',
     # Students urls
     url(r'^$', 'students.views.students.students_list', name='home'),
     url(r'^students/add/$', 'students.views.students.students_add',name='students_add'),
-    url(r'^students/(?P<sid>\d+)/edit/$','students.views.students.students_edit',name='students_edit'),
+    #url(r'^students/(?P<sid>\d+)/edit/$','students.views.students.students_edit',name='students_edit'),
     url(r'^students/(?P<sid>\d+)/delete/$','students.views.students.students_delete',name='students_delete'),
 
     # Groups urls
@@ -14,7 +19,6 @@ urlpatterns = patterns('',
     url(r'^groups/add/$', 'students.views.groups.groups_add',name='groups_add'),
     url(r'^groups/(?P<gid>\d+)/edit/$','students.views.groups.groups_edit',name='groups_edit'),
     url(r'^groups/(?P<gid>\d+)/delete/$','students.views.groups.groups_delete',name='groups_delete'),
-    url(r'^admin/', include(admin.site.urls)),
 
     # Journal urls
     url(r'^journal/$', 'students.views.journal.journal_list', name='journal'),
@@ -23,8 +27,13 @@ urlpatterns = patterns('',
     url(r'^exam/$', 'students.views.exam.exam_list', name='exam'),
 
     #Contact admin Form
-    url(r'^contact-admin/$','students.views.contact_admin.contact_admin', name='contact_admin'),
+    #url(r'^contact-admin/$','students.views.contact_admin.contact_admin', name='contact_admin'),
+    url(r'^contact-admin/$',ContactView.as_view(),name='contact_admin'),
+    url(r'^admin/', include(admin.site.urls)),
 
+    #Students edit
+    url(r'^students/(?P<pk>\d+)/edit/$',StudentUpdateView.as_view(),name='students_edit'),
+    
 )
 if DEBUG:
     # serve files from media folder
