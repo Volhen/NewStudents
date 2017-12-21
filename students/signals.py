e871b2b4a2122046e836e8d87fe9c88b11f5f59d
Alexzev <alex@Alezev>
2017-12-21 15:30:43 +0300

    21.12.2017-signals(home work)

--- students/signals.py
+++ students/signals.py
@@ -4,9 +4,9 @@ import logging
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import Student
+from .models import Student,Group
 
-
+#***Student signals********
 @receiver(post_save, sender=Student)
 def log_student_updated_added_event(sender, **kwargs):
     """Writes information about newly added or updated student into log file"""
@@ -27,3 +27,23 @@ def log_student_deleted_event(sender, **kwargs):
 
     student = kwargs['instance']
     logger.info("Student deleted: %s %s(ID: %d)",student.first_name, student.last_name,student.id)
+
+#***Grups signals********
+@receiver(post_save, sender=Group)
+def log_group_updated_added_event(sender, **kwargs):
+    """Writes information about newly added or updated group into log file"""
+    logger = logging.getLogger(__name__)
+
+    group = kwargs['instance']
+    if kwargs['created']:
+        logger.info("Group added: %s %s (ID: %d)", group.title, group.leader.first_name,group.id)
+    else:
+        logger.info("Group updated: %s %s (ID: %d)", group.title, group.leader.first_name,group.id)
+
+@receiver(post_delete, sender=Group)
+def log_group_deleted_event(sender, **kwargs):
+    """Writes information about deleted student into log file"""
+    logger = logging.getLogger(__name__)
+
+    group = kwargs['instance']
+    logger.info("Group deleted: %s %s(ID: %d)",group.title, group.leader.first_name,group.id)
